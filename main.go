051f74ed55427ba0233b6f00224ab6ef88927d7e
Vera <63463936+lveral@users.noreply.github.com>
2024-07-01 23:16:28 +0500

    first commit

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var startup string = "Textio SMS service booting up..."
+	var message string = "Sending test message"
+	var confirmation string = "Message sent!"
+
+	// don't touch below this line
+
+	fmt.Println(startup)
+	fmt.Println(message)
+	fmt.Println(confirmation)
 }
