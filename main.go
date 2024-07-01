49903818d9a7ca75e51005a2a7ea6dce99705613
Vera <63463936+lveral@users.noreply.github.com>
2024-07-01 23:22:38 +0500

    ch2 BASIC TYPES

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	// initialize variables here
 	var smsSendingLimit int
 	var costPerSMS float64
-	var hasPermission int
+	var hasPermission bool
 	var username string
 
 	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
