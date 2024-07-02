ad0110c7971930141f360a4d79571f3e5b9c04f4
Vera <63463936+lveral@users.noreply.github.com>
2024-07-02 22:09:07 +0500

    ch3 PASSING VARIABLES BY VALUE unit-test

--- main.go
+++ main.go
@@ -3,8 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	penniesPerText := 2.0
+	fmt.Print("Test Passed: (2, 89, 102) -> expected: 26 actual: ")
+	fmt.Print(monthlyBillIncrease(2, 89, 102))
+	fmt.Print("\nTest Passed: (2, 98, 104) -> expected: 12 actual: ")
+	fmt.Print(monthlyBillIncrease(2, 98, 104))
+	fmt.Print("\nTest Passed: (3, 50, 40) -> expected: -30 actual: ")
+	fmt.Print(monthlyBillIncrease(3, 50, 40))
+	fmt.Print("\nTest Passed: (3, 60, 60) -> expected: 0 actual: ")
+	fmt.Print(monthlyBillIncrease(3, 60, 60))
+}
 
-	// don't edit below this line
-	fmt.Printf("The type of penniesPerText is %T\n", penniesPerText)
+func monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth int) int {
+	var lastMonthBill int
+	var thisMonthBill int
+	lastMonthBill = getBillForMonth(costPerSend, numLastMonth)
+	thisMonthBill = getBillForMonth(costPerSend, numThisMonth)
+	return thisMonthBill - lastMonthBill
+}
+
+func getBillForMonth(costPerSend, messagesSent int) int {
+	return costPerSend * messagesSent
 }
