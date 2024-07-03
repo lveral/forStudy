0da7d5a1c3a5c461ba9d4eb86578ad79d8c1cb3b
Vera <63463936+lveral@users.noreply.github.com>
2024-07-03 22:45:43 +0500

    ch4 SEND MESSAGE + unit-test

--- main.go
+++ main.go
@@ -5,6 +5,23 @@ import (
 	"testing"
 )
 
+type User struct {
+	Name string
+	Membership
+}
+
+func (user User) SendMessage(message string, messageLength int) (string, bool) {
+	if user.MessageCharLimit >= messageLength {
+		return message, true
+	}
+	return "", false
+}
+
+type Membership struct {
+	Type             membershipType
+	MessageCharLimit int
+}
+
 type membershipType string
 
 const (
@@ -12,32 +29,14 @@ const (
 	TypePremium  membershipType = "premium"
 )
 
-// don't touch above this line
-
-type Membership struct {
-	Type             membershipType
-	MessageCharLimit int
-}
-
-type User struct {
-	Name string
-	Membership
-}
-
 func newUser(name string, membershipType membershipType) User {
-	var limit int
+	membership := Membership{Type: membershipType}
 	if membershipType == TypeStandard {
-		limit = 100
-	} else {
-		limit = 1000
+		membership.MessageCharLimit = 100
+	} else if membershipType == TypePremium {
+		membership.MessageCharLimit = 1000
 	}
-	return User{
-		Name: name,
-		Membership: Membership{
-			Type:             membershipType,
-			MessageCharLimit: limit,
-		},
-	}
+	return User{Name: name, Membership: membership}
 }
 
 func main() {
@@ -46,32 +45,45 @@ func main() {
 }
 
 func Test(t *testing.T) {
+	type expectedCase struct {
+		expectedMessage string
+		result          bool
+	}
 	type testCase struct {
-		name           string
-		membershipType membershipType
-		expected       User
+		message       string
+		messageLength int
+		user          User
+		expectedCase
 	}
 
 	tests := []testCase{
-		{"Name1", TypeStandard, User{Name: "Name1", Membership: Membership{TypeStandard, 100}}},
-		{"Name1", TypePremium, User{Name: "Name1", Membership: Membership{TypePremium, 1000}}},
+		{"message1", 100, User{Name: "Name1", Membership: Membership{TypeStandard, 100}}, expectedCase{"message1", true}},
+		{"message2", 110, User{Name: "Name1", Membership: Membership{TypeStandard, 100}}, expectedCase{"", false}},
+		{"message3", 1000, User{Name: "Name1", Membership: Membership{TypePremium, 1000}}, expectedCase{"message3", true}},
+		{"message4", 1100, User{Name: "Name1", Membership: Membership{TypePremium, 1000}}, expectedCase{"", false}},
 	}
 
 	for _, test := range tests {
-		if output := newUser(test.name, test.membershipType); output != test.expected {
+		if output, res := test.user.SendMessage(test.message, test.messageLength); output != test.expectedMessage || res != test.result {
 			t.Errorf(
-				"Test Failed: (%v, %v) -> expected: %v actual: %v",
-				test.name,
-				test.membershipType,
-				test.expected,
+				"Test Failed: (%v, %v, %v) -> expected: %v, %v actual: %v, %v",
+				test.message,
+				test.messageLength,
+				test.user,
+				test.expectedMessage,
+				test.result,
 				output,
+				res,
 			)
 		} else {
-			fmt.Printf("Test Passed: (%v, %v) -> expected: %v actual: %v\n",
-				test.name,
-				test.membershipType,
-				test.expected,
+			fmt.Printf("Test Passed: (%v, %v, %v) -> expected: %v, %v actual: %v, %v\n",
+				test.message,
+				test.messageLength,
+				test.user,
+				test.expectedMessage,
+				test.result,
 				output,
+				res,
 			)
 		}
 	}
