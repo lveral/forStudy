c98719433ab72b798e415dafe81c460176dba0e6
Vera <63463936+lveral@users.noreply.github.com>
2024-07-06 00:26:38 +0500

    ch5 USER INPUT

--- main.go
+++ main.go
@@ -1,50 +1,27 @@
 package main
 
-type notification interface {
-	importance() int
-}
+import (
+	"errors"
+	"fmt"
+)
 
-type directMessage struct {
-	senderUsername string
-	messageContent string
-	priorityLevel  int
-	isUrgent       bool
-}
-
-func (d directMessage) importance() int {
-	if d.isUrgent {
-		return 50
+func validateStatus(status string) error {
+	if status == "" {
+		return errors.New("status cannot be empty")
 	}
-	return d.priorityLevel
-}
-
-type groupMessage struct {
-	groupName      string
-	messageContent string
-	priorityLevel  int
-}
-
-func (gm groupMessage) importance() int {
-	return gm.priorityLevel
-}
-
-type systemAlert struct {
-	alertCode      string
-	messageContent string
-}
-
-func (sa systemAlert) importance() int {
-	return 100
-}
-
-func processNotification(n notification) (string, int) {
-	switch v := n.(type) {
-	case directMessage:
-		return v.senderUsername, n.importance()
-	case groupMessage:
-		return v.groupName, n.importance()
-	case systemAlert:
-		return v.alertCode, n.importance()
+	if len(status) > 140 {
+		return errors.New("status exceeds 140 characters")
 	}
-	return "", 0
+	return nil
+}
+
+func main() {
+	tests := []string{
+		"",
+		"normal status",
+		"very big status very big status very big status very big status very big status very big status very big status very big status very big status ",
+	}
+	for _, test := range tests {
+		fmt.Println(validateStatus(test))
+	}
 }
